6ebc4bcc64774e369a9a3cd6b9e97a36ee6d7664
PProvost <peter@provost.org>
2010-04-29 01:44:26 -0600

    Reenabled whisper add code

--- OptionsPage.lua
+++ OptionsPage.lua
@@ -38,7 +38,6 @@ function ns:CreateOptionsPage(parent)
 	end)
 	showMinimap:SetChecked(not ns.db.minimap.hide)
 
-	--[[
 	local enableWhisperAdd = tekcheck.new(parent, nil, "Enable Whisper Add", "TOPLEFT", showMinimap, "BOTTOMLEFT", 0, -VGAP)
 	enableWhisperAdd.tiptext = "Enables people to whisper you \"!add\" (followed optionally by a role) to be added to the current roster."
 	orig = enableWhisperAdd:GetScript("OnClick")
@@ -46,7 +45,6 @@ function ns:CreateOptionsPage(parent)
 		ns.db.enableWhisperAdd = not ns.db.enableWhisperAdd
 	end)
 	enableWhisperAdd:SetChecked(ns.db.enableWhisperAdd)
-	]]
 end
 
 
